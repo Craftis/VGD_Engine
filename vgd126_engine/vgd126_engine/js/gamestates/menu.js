489633da7ad58f1c4947d1babbf2d6981a621099
Craftis <paletteblade@gmail.com>
2026-09-10 11:00:23 -0400

    Updated menu sprites

--- vgd126_engine/vgd126_engine/js/gamestates/menu.js
+++ vgd126_engine/vgd126_engine/js/gamestates/menu.js
@@ -4,8 +4,8 @@
  */
 
 var startButton = new GameObject();
-startButton.width = 200;
-startButton.hitBoxWidth = 800;
+startButton.width = 256;
+startButton.hitBoxWidth = 500;
 
 var menuBackground = new GameObject();
 
@@ -13,7 +13,7 @@ var menuBackground = new GameObject();
    ✏️ STUDENT EDIT ZONE: MENU BACKGROUND IMAGE
    Replace with your own JPG/PNG; keep aspect ratio close to canvas.
    ===================================================================== */
-menuBackground.img.src = "images/mrt.jpg";
+menuBackground.img.src = "images/Sprite-menu.png";
 menuBackground.width = canvas.width;
 menuBackground.height = canvas.height;
 
@@ -21,11 +21,11 @@ gameStates[`menu`] = function () {
   // Hover & click
   if (startButton.overlap(mouse)) {
     if (mouse.pressed) { gameStates.changeState(`level1`); sounds.play(`bgm`,0, loop = true);}
-    startButton.color = `yellow`;
+    startButton.img.src = "images/Sprite-buttonON.png";
   } else {
-    startButton.color = `red`;
+    startButton.img.src = "images/Sprite-buttonOFF.png";
   }
 
   menuBackground.drawStaticImage();
-  startButton.render();
+  startButton.drawStaticImage();
 };
